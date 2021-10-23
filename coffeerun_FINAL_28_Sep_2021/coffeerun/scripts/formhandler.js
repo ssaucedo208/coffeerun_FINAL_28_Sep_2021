e5d69276f49289f4cca1740462348d8fe57edf87
ssaucedo208 <46984431+ssaucedo208@users.noreply.github.com>
2021-10-22 18:08:17 -0700

    Add files via upload

--- coffeerun_FINAL_28_Sep_2021/coffeerun/scripts/formhandler.js
+++ coffeerun_FINAL_28_Sep_2021/coffeerun/scripts/formhandler.js
@@ -5,43 +5,40 @@
 
         class FormHandler {
             constructor(selector) {
-                if (!selector) {
-                    throw new Error('No selector provided');
-                }
+                if (!selector) { throw new Error('No selector provided'); }
 
                 this.$formElement = $(selector);
-                if (this.$formElement.length === 0) {
+                if (this.$formElement.length === 0) { 
                     throw new Error('Could not find element with selector: ' + selector);
                 }
             }
 
-            addSubmitHandler(fn) {
+            addSubmitHandler(fn) { 
                 console.log('Setting submit handler for form');
-                this.$formElement.on('submit', function (event) {
+                this.$formElement.on('submit', function(event) { 
                     event.preventDefault();
 
                     // var data = $(this).serializeArray();
                     var data = {};
-                    $(this).serializeArray().forEach(function (item) {
+                    $(this).serializeArray().forEach(function(item) { 
                         data[item.name] = item.value;
                         console.log(item.name + ' is ' + item.value);
                     });
                     console.log(data);
                     fn(data)
-                        .then(function () {
+                        .then(function() {
                             this.reset();
                             this.elements[0].focus();
                         }.bind(this));
                 });
             }
-            
-            addInputHandler(fn) {
+            addInputHandler(fn) { 
                 console.log('Setting input handler for form');
-                this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+                this.$formElement.on('input', '[name="emailAddress"]', function(event) {
                     var emailAddress = event.target.value;
                     var message = '';
                     // console.log(fn(emailAddress));
-                    if (fn(emailAddress)) {
+                    if (fn(emailAddress)) { 
                         event.target.setCustomValidity('');
                     } else {
                         message = emailAddress + ' is not authorized!';
@@ -50,31 +47,7 @@
                 });
             }
         }
-
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+        
         App.FormHandler = FormHandler;
         window.App = App;
-    })(window);
+      })(window);
